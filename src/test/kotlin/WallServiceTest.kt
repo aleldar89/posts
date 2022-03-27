d24e80ce9d78adad2d93ade294205816c4bd8437
Эльдар Алиев <alel29643@gmail.com>
2022-03-27 21:43:12 +0300

    правка ошибок

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -4,34 +4,30 @@ import org.junit.Assert.*
 
 class WallServiceTest {
 
-    private val firstPost = Post(id = 1, ownerId = 1, text = "первый пост")
-    private val secondPost = Post(id = 2, ownerId = 2, text = "второй пост")
-    private val thirdPost = Post(id = 3, ownerId = 3, text = "третий пост")
-
     @Test
     fun addIdNotNull() {
         val service = WallService
-        val result = service.add(firstPost)
+        val result = service.add(Post(text = "первый пост"))
         assertNotNull(result)
     }
 
     @Test
     fun updateSuccessful() {
         val service = WallService
-        service.add(firstPost)
-        service.add(secondPost)
-        service.add(thirdPost)
-        val result = service.update(Post(id = 1, ownerId = 100, text = "обновленный пост"))
+        service.add(Post(text = "первый пост"))
+        service.add(Post(text = "второй пост"))
+        service.add(Post(text = "третий пост"))
+        val result = service.update(Post(id = 1, text = "обновленный пост"))
         assertTrue(result)
     }
 
     @Test
     fun updateNotSuccessful() {
         val service = WallService
-        service.add(firstPost)
-        service.add(secondPost)
-        service.add(thirdPost)
-        val result = service.update(Post(id = 4, ownerId = 100, text = "обновленный пост"))
+        service.add(Post(text = "первый пост"))
+        service.add(Post(text = "второй пост"))
+        service.add(Post(text = "третий пост"))
+        val result = service.update(Post(id = 4, text = "обновленный пост"))
         assertFalse(result)
     }
 
