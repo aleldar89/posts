427336b5bb2a0fd0444634c23cb45723bd1a9895
Эльдар Алиев <alel29643@gmail.com>
2022-05-07 01:21:09 +0300

    Sequances

--- src/main/kotlin/chat/chat.kt
+++ src/main/kotlin/chat/chat.kt
@@ -2,7 +2,7 @@ package chat
 
 data class Chat(
     var chatId: Int = 0,
-    var chatMessages: Array<Message> = emptyArray(),
+    var chatMessages: MutableList<Message> = mutableListOf(),
     var messageCount: Int = 0,
     var unreadMessageCount: Int = 0,
     var readed: Boolean = false
--- src/test/kotlin/ChatserviceTest.kt
+++ src/test/kotlin/ChatserviceTest.kt
@@ -7,15 +7,20 @@ class ChatserviceTest {
     private val chatService = Chatservice
 
     @Test
-    fun deleteChatShouldNotThrow() {
+    fun sendMessageOrCreateChat() {
+        chatService.clearChatservice()
         chatService.sendMessage(1, NewMessage(2, "hello"))
+        chatService.sendMessage(1, NewMessage(2, "how do you do?"))
+        chatService.sendMessage(2, NewMessage(3, "hi"))
+    }
+
+    @Test
+    fun deleteChatShouldNotThrow() {
         chatService.deleteChat(1, 2)
     }
 
     @Test (expected = NonExistentChat::class)
     fun deleteChatShouldThrow()  {
-        chatService.clearChatservice()
-        chatService.sendMessage(1, NewMessage(2, "hello"))
         chatService.deleteChat(1, 3)
     }
 
@@ -23,41 +28,48 @@ class ChatserviceTest {
     fun getChats() {
         chatService.clearChatservice()
         chatService.sendMessage(1, NewMessage(2, "hello"))
-        chatService.sendMessage(1, NewMessage(3, "hi"))
         chatService.getChats(1)
     }
 
     @Test
-    fun sendMessageOrCreateChat() {
-        chatService.clearChatservice()
-        chatService.sendMessage(1, NewMessage(2, "hello"))
-        chatService.sendMessage(1, NewMessage(2, "how do you do?"))
-        chatService.sendMessage(1, NewMessage(2, "my name is anon"))
-        chatService.sendMessage(2, NewMessage(3, "hi"))
-    }
-
-    @Test
-    fun deleteMessageShouldNotThrow() {
-        chatService.clearChatservice()
-        chatService.sendMessage(1, NewMessage(2, "hello"))
-        chatService.deleteMessage(1,2,Message(1))
-    }
-
-    @Test (expected = RuntimeException::class)
-    fun deleteMessageShouldThrow() {
-        chatService.clearChatservice()
-        chatService.sendMessage(1, NewMessage(2, "hello"))
-        chatService.deleteMessage(1,2,Message(2))
-    }
-
-    @Test
-    fun getMessages() {
+    fun getMessagesShouldNotThrow() {
         chatService.clearChatservice()
         chatService.sendMessage(1, NewMessage(2, "hello"))
         chatService.sendMessage(1, NewMessage(2, "how do you do?"))
         chatService.sendMessage(1, NewMessage(2, "my name is anon"))
         chatService.sendMessage(1, NewMessage(2, "do you hear me?"))
-        chatService.getMessages(1,1,4)
+        chatService.getMessage(1, 1, 4)
+    }
+
+    @Test (expected = RuntimeException::class)
+    fun getMessagesShouldThrow() {
+        chatService.clearChatservice()
+        chatService.sendMessage(1, NewMessage(2, "hello"))
+        chatService.sendMessage(1, NewMessage(2, "how do you do?"))
+        chatService.sendMessage(1, NewMessage(2, "my name is anon"))
+        chatService.sendMessage(1, NewMessage(2, "do you hear me?"))
+        chatService.getMessage(0, 1, 4)
+    }
+
+    @Test
+    fun deleteSuccesful() {
+        chatService.clearChatservice()
+        chatService.sendMessage(1, NewMessage(2, "hello"))
+        chatService.deleteMessage(1, 2, Message(1))
+    }
+
+    @Test (expected = RuntimeException::class)
+    fun deleteWrongMessage() {
+        chatService.clearChatservice()
+        chatService.sendMessage(1, NewMessage(2, "hello"))
+        chatService.deleteMessage(1, 2, Message(2))
+    }
+
+    @Test (expected = RuntimeException::class)
+    fun deleteFromWrongChat() {
+        chatService.clearChatservice()
+        chatService.sendMessage(1, NewMessage(2, "hello"))
+        chatService.deleteMessage(0, 2, Message(1))
     }
 
 }
